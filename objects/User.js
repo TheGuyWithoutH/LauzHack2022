809f0f575482d83cdd4dfa11e0fc35c10a93aafe
Menelik Nouvellon <meneliknouvellon@gmail.com>
2022-12-04 02:18:46 +0100

    Post working

--- objects/User.js
+++ objects/User.js
@@ -1,11 +1,15 @@
 import { deleteUser, EmailAuthProvider, getAuth, reauthenticateWithCredential, reauthenticateWithPopup, sendEmailVerification, signOut,updateEmail,updatePassword, updateProfile } from "firebase/auth"
+import { arrayUnion, collection, doc, getDoc, getFirestore, setDoc, updateDoc } from "firebase/firestore"
+import { COLLECTIONS } from "../Constants"
 import { AbstractUser } from "./AbstractUser"
+import { postConverter } from "./Post"
 
 
 
 export class User extends AbstractUser{
     #uid
     user
+
 
     constructor(uid, user=null) {
         super()
@@ -32,6 +36,22 @@ export class User extends AbstractUser{
      */
     logout() { 
         return signOut(getAuth())
+    }
+
+    getPersonalInformation(){
+        return getDoc(doc(getFirestore(), COLLECTIONS.REGULAR_USERS,this.#uid)).then(doc => doc.data())
+    }
+
+    
+    post(post) {
+        const col = collection(getFirestore(), COLLECTIONS.AVAILABLE_OBJECTS)
+        const postRef = doc(col, post.getId()).withConverter(postConverter)
+        console.log("CHECK")
+        setDoc(postRef, post)
+        const userRef = doc(getFirestore(), COLLECTIONS.REGULAR_USERS, this.#uid)
+        return updateDoc(userRef, {myPosts : arrayUnion(post.asDataObject())}).catch(err => {
+            return setDoc(userRef, {myPosts : arrayUnion(post.asDataObject())})
+        })
     }
 
 }
